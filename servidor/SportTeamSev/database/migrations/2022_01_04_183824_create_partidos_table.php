21220d8a980c4baf0794df36f362e24d66d32179
Enrique Sánchez Vicente <82119845+EnriqueSanVic@users.noreply.github.com>
2022-01-04 19:50:49 +0100

    creacion de rutas para web

---
+++ servidor/SportTeamSev/database/migrations/2022_01_04_183824_create_partidos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partidos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partidos');
+    }
+}
