184fa088b74229003059757c70b2c05a59ead835
adrian <adri.trinitarias@gmail.com>
2022-01-17 14:14:52 +0100

    script de cifrado

--- servidor/SportTeamSev/public/js/codificadorContra.js
+++ servidor/SportTeamSev/public/js/codificadorContra.js
@@ -1,8 +1,10 @@
-function generateHash() {
+async function generateHash() {
+    alert("kek");
+    console.log(document.getElementById('plainText'));
     var plainText = document.getElementById('plainText').value;
     var md = forge.md.sha256.create();
     md.start();
     md.update(plainText, "utf8");
     var hashText = md.digest().toHex();
-    document.getElementById("hashText").innerHTML = hashText;
+    return hashText;
 }
